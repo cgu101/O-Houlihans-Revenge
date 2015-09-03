e1d3a22a934e05df622839a2c7c648d4d0788c10
Connor Usry <cgu1001@gmail.com>
2015-09-03 15:10:24 -0400

    Revamps main menu look

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,15 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -68,31 +72,38 @@ public class Main extends Application {
 
     //sets scene to main Menu
     public void makeMainMenu() {
+        //Menu Image
+        Image menuImage = new Image(getClass().getClassLoader().getResourceAsStream("main/resources/images/patchesMenu.png"));
+        ImageView menuIV = new ImageView(menuImage);
+
+        //Menu Options
         BorderPane mainMenuPane = new BorderPane();
         Label welcomeLbl = new Label("O'Houlihan's Revenge... Pick Your Challenge");
+        welcomeLbl.setTextFill(Color.ANTIQUEWHITE);
 
-        //Select Your Training Level
         Button trainingButton = new Button("Begin your training!");
         trainingButton.setOnAction(e -> makeTrainingLevel());
         Button battleButton = new Button("Battle Goodman!");
         battleButton.setOnAction(e -> makeBattleLevel());
 
-        // Quit the game
         Button quitButton = new Button("Quit-I hate fun");
         quitButton.setOnAction(e -> Platform.exit());
+        quitButton.setLayoutX(menuIV.getBoundsInParent().getWidth() - 120);
+        quitButton.setLayoutY(menuIV.getBoundsInParent().getHeight() - 50);
 
         //Set up the main menu for level selection
+        Group root = new Group();
         VBox gameTypesVbox = new VBox(20);
         gameTypesVbox.setAlignment(Pos.CENTER);
         gameTypesVbox.getChildren().addAll(welcomeLbl, trainingButton, battleButton);
         lives = 3;
 
         mainMenuPane.setCenter(gameTypesVbox);
-        mainMenuPane.setBottom(quitButton);
         mainMenuPane.setAlignment(quitButton, Pos.BOTTOM_RIGHT);
-        mainMenuPane.setStyle("-fx-background-color: red;");
 
-        Scene mainMenuScene = new Scene(mainMenuPane, WIDTH_SIZE, HEIGHT_SIZE);
+        root.getChildren().addAll(menuIV, mainMenuPane, quitButton);
+
+        Scene mainMenuScene = new Scene(root, menuIV.getBoundsInParent().getWidth(), menuIV.getBoundsInParent().getHeight());
         primaryStage.setScene(mainMenuScene);
         primaryStage.show();
     }
